8573a21a71f7a6e60206be874d0ee759f0957aa6
Bryan Stone <aegixx@gmail.com>
2019-03-28 11:54:49 -0500

    Add Brewfile for brew bundle
    Signed-off-by: Bryan Stone <aegixx@gmail.com>

--- build-my-mac.sh
+++ build-my-mac.sh
@@ -7,97 +7,18 @@ if [ ! command -v brew >/dev/null 2>&1 ]; then
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-brew tap caskroom/drivers
-brew tap caskroom/versions
+# Use `brew bundle dump` to get `Brewfile`
+brew upgrade
+brew cleanup
 
-brew cask install \
-bit-slicer \
-aws-vault \
-intellij-idea-ce \
-steam \
-tower \
-atom \
-microsoft-office \
-google-chrome \
-goland \
-docker \
-iterm2 \
-istat-menus \
-adobe-acrobat-reader \
-adobe-creative-cloud \
-vnc-viewer \
-rubymine \
-caffeine \
-minikube \
-discord \
-java \
-alfred \
-xmind \
-slack \
-spectacle \
-google-backup-and-sync \
-parallels \
-appcleaner \
-mysqlworkbench \
-flux \
-postman \
-logitech-options \
-quicken \
-quickbooks-online \
-little-snitch \
-micro-snitch \
-teamviewer \
-evernote \
-nvidia-geforce-now \
-virtualbox \
-malwarebytes \
-bitbar \
-microsoft-intellitype \
-gimp
-
-brew install \
-htop \
-wget \
-nmap \
-nodejs \
-geoip \
-go \
-tree \
-watch \
-zsh \
-git \
-coreutils \
-zsh-completions \
-zsh-syntax-highlighting \
-gpg \
-awscli \
-awsebcli \
-awslogs \
-aws-elasticbeanstalk \
-jq \
-mysql \
-python \
-mas \
-kubernetes-helm \
-jenv \
-php
-
-mas install 497799835  # XCode
-mas install 926036361  # Lastpass
-mas install 1295203466 # Microsoft Remote Desktop
-mas install 410628904  # Wunderlist
-mas install 1208561404 # Kaspersky VPN
-mas install 1176895641 # Spark
-mas install 1278508951 # Trello
-mas install 430798174  # Hazeover
-mas install 1142578753 # Omnigraffle
+brew bundle
 
 # Zsh
 sudo dscl . -create /Users/$USER UserShell /usr/local/bin/zsh
 # Oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 # RVM
-gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 \curl -sSL https://get.rvm.io | bash -s stable --ruby
 
 # kymsu (https://github.com/welcoMattic/kymsu)
